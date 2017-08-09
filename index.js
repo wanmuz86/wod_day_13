6c6b11d25c6350784baf6887428fab95a8da191d
faizalrazak <mohamadfaizalrazak@gmail.com>
2017-08-09 15:32:45 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function marks(x){
+  var result;
+  
+  for(var i=0; i<x.length; i++){
+    document.write(x[i]);
+    if(x[i] <38){
+      result = x[i];
+    }else{
+      var a = x[i] +  1;
+      var b = x[i] + 2;
+      
+      if( a % 5 == 0){
+        result = a;
+      }else if( b % 5 == 0){
+        result = b;
+      }else{
+        result = x[i];
+      }
+    }
+    document.write(" = "+result+"<br>");
+  }
+  
+}
+marks([37,58,41]);
